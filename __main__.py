e6861f4b559a09b6ccd90e510bfe6146d8cc72c9
A2drien <adriendubois1411@gmail.com>
2022-10-20 15:37:56 +0200

    Fin Question 1a)

--- __main__.py
+++ __main__.py
@@ -1,16 +1,16 @@
-from email import utils
-import pandas
+import pandas as pd
 from utils_project import *
 
 if __name__ == '__main__':
-    dfAliments = pandas.read_excel(FICHIER_ALIMENTS)
-    dfSondage = pandas.read_excel(FICHIER_SONDAGE)
-
+    dfAliments = pd.read_excel(FICHIER_ALIMENTS)
+    dfSondage = pd.read_excel(FICHIER_SONDAGE)
+    
     for nomPreference in PREFERENCES_ALIMENTAIRES.keys():
+        #Ajout pour chaque aliment de leur appartenance ou non à toutes les catégories
         dfAliments[nomPreference] = correspondanceAlimentaire(dfAliments, nomPreference)
 
-    # Facultatif, sert au debug
-    dfAliments.to_excel('Aliment-modif.xlsx', index=False)
-
-    for nomPreference in PREFERENCES_ALIMENTAIRES.keys():
-        dfSondage["nb" + nomPreference] = [getNbTypeAliment(personne, dfAliments, nomPreference) for personne in dfSondage.values]
+        #Ajout pour chaque sondé, du nombre d'aliments consommés par catégorie
+        dfSondage["nb" + nomPreference] = getNbTypeAlimentsCategorieGlobal(dfSondage, dfAliments, nomPreference)
+        
+        #Sauvegarde du fichier créé
+        listePreferenceAlimentaire(dfSondage, nomPreference)
--- utils_project.py
+++ utils_project.py
@@ -1,6 +1,5 @@
-import string
 from pandas import DataFrame
-
+from os import path, makedirs
 FICHIER_ALIMENTS = "Aliments.xlsx"
 FICHIER_SONDAGE = "Sondage.xlsx"
 
@@ -9,6 +8,9 @@ _NOM_COLONNE_SPECIFICATION_ALIMENTS = 'alim_ssssgrp_nom_fr'
 _NOM_COLONNE_NUM_PRODUIT = 'alim_code'
 _NOM_COLONNE_NB_ALIMENTS = 'Administré.e'
 
+_DONNEES_SONDE: list[str] = ["Nom", "Prénom", "Tel"]
+
+_NOM_DOSSIER_Q1a = 'Question-1a'
 
 PREFERENCES_ALIMENTAIRES: dict[str, tuple[str, list[str], list[str]]] = {
     "Bio":      (
@@ -61,10 +63,10 @@ def _estEtat(dfAliments: DataFrame, idProduct: int, nomColonne: str) -> bool:
     Returns:
         bool: Valeur de la colonne (et donc de l'état recherché)
     """
-    return dfAliments[nomColonne][_getNumLigneProduct(dfAliments, idProduct)].to_lower() == 'true'
+    return bool(dfAliments[nomColonne].iloc[_getNumLigneProduct(dfAliments, idProduct)])
 
 
-def _getListeAliment(dfSondage: DataFrame, idxSonde: int) -> list[int]:
+def _getListeAliments(dfSondage: DataFrame, idxSonde: int) -> list[int]:
     """Revoie la liste des numéro de produits indiqués par le sondé
 
     Args:
@@ -74,25 +76,26 @@ def _getListeAliment(dfSondage: DataFrame, idxSonde: int) -> list[int]:
     Returns:
         list[int]: Liste des numéros de produits
     """
-    nbAliments = int(dfSondage[_NOM_COLONNE_NB_ALIMENTS][idxSonde])
+    nbAliments = int(dfSondage[_NOM_COLONNE_NB_ALIMENTS].iloc[idxSonde])
     listeAliments = []
 
     while nbAliments > 0:
-        listeAliments.append(dfSondage[f'Aliment{nbAliments}'][idxSonde])
+        listeAliments.append(dfSondage[f'Aliment{nbAliments}'].iloc[idxSonde])
+        nbAliments -= 1
 
     return listeAliments
 
 
-def getNbTypeAliment(listeIdProduct: list[int], dfAliments: DataFrame, colonneTypeAliment: str):
-    """Retourne le
+def _getNbTypeAlimentCategorieIndividuel(listeIdProduct: list[int], dfAliments: DataFrame, colonneTypeAliment: str) -> int:
+    """Renvoie le nombre d'aliments consommé dans une catégorie précisée
 
     Args:
-        listeIdProduct (list[int]): _description_
-        dfAliments (DataFrame): _description_
-        colonneTypeAliment (str): _description_
+        listeIdProduct (list[int]): Liste des aliments consommés
+        dfAliments (DataFrame): DataFrame des aliments
+        colonneTypeAliment (str): Nom de la catégorie recherchée
 
     Returns:
-        _type_: _description_
+        int: Nombre d'aliments de la catégorie recherchée
     """
     nbOccurrence = 0
     for idProduct in listeIdProduct:
@@ -110,11 +113,43 @@ def _supprimerExceptions(string: str, exceptions: list[str]) -> str:
 def correspondanceAlimentaire(dfAliment: DataFrame, nomPreference: str) -> list[bool]:
     nomColonne, correspondances, exceptions = PREFERENCES_ALIMENTAIRES[nomPreference]
 
-    L: list[bool] = []
+    L = []
     for label in dfAliment[nomColonne]:
         if any(correspondance in _supprimerExceptions(label.lower(), exceptions) for correspondance in correspondances):
             L.append(True)
-            print(f"{nomPreference} : {label}")
         else:
             L.append(False)
     return L
+
+
+def getNbTypeAlimentsCategorieGlobal(dfSondage: DataFrame, dfAliments: DataFrame, nomCategorie: str) -> list[int]:
+    """Retourne, pour tous les sondés, le nombre d'aliments correspondant à la catégorie demandée
+
+    Args:
+        dfSondage (DataFrame): DataFrame des sondés
+        dfAliments (DataFrame): DataFrame des aliments
+        nomCategorie (str): Nom de la catégorie des aliments à rechercher
+
+    Returns:
+        list[int]: Nombre d'aliments correspondant à la catégorie demandée
+    """
+    listeNbTypeAlimentsCategorieGlobal = []
+
+    for idxSonde in range(len(dfSondage.values)):
+        listeAliments = _getListeAliments(dfSondage, idxSonde)
+        nbTypeAlimentsCategorieGlobal = _getNbTypeAlimentCategorieIndividuel(
+            listeAliments, dfAliments, nomCategorie)
+        listeNbTypeAlimentsCategorieGlobal.append(
+            nbTypeAlimentsCategorieGlobal)
+
+    return listeNbTypeAlimentsCategorieGlobal
+
+
+def creerDossier(nomDossier: str) -> None:
+    if not path.exists(nomDossier):
+        makedirs(nomDossier)
+
+
+def listePreferenceAlimentaire(dfSondage: DataFrame, nomCategorie: str) -> None:
+    creerDossier(_NOM_DOSSIER_Q1a)
+    dfSondage.to_excel(f"{_NOM_DOSSIER_Q1a}/Pref-{nomCategorie}.xlsx", index=False, columns= _DONNEES_SONDE + ["nb"+nomCategorie])
